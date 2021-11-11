d8ecf01ca5b1bae158e61eecd67a94e6a4518370
JinYeongKim <handayoung11@gmail.com>
2021-11-12 01:20:39 +0900

    연관 엔티티 지연로딩으로 설정 및 연관관계 편의 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -23,10 +23,15 @@ public class Category {
     )
     private List<OrderItem> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
