fa1a31e3ac05c297c8b26d0de820254b8f8e94bf
JinYeongKim <handayoung11@gmail.com>
2021-11-17 22:23:24 +0900

    Album, Book, Movie에 Getter 및 Setter 추가

--- src/main/java/jpabook/jpashop/domain/Item/Album.java
+++ src/main/java/jpabook/jpashop/domain/Item/Album.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.Item;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-@Entity
+@Entity @Data
+@EqualsAndHashCode(callSuper = true)
 @DiscriminatorValue("A")
 public class Album extends Item {
 
