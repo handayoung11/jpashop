56216a9a8f35035b7b8f16b3e71e5b230cafe19c
JinYeongKim <handayoung11@gmail.com>
2021-12-06 09:46:24 +0900

    아이템 등록 기능 작성

---
+++ src/main/java/jpabook/jpashop/dto/BookForm.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.dto;
+
+import lombok.Data;
+
+@Data
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
