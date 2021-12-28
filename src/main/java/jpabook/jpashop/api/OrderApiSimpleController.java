8340a6ba56bcf4e3807477799bbb3a6e36d08f45
JinYeongKim <handayoung11@gmail.com>
2021-12-28 15:28:57 +0900

    주문조회 API 작성(v2: DTO 반환)

---
+++ src/main/java/jpabook/jpashop/api/OrderApiSimpleController.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiSimpleController {
+    private final OrderRepository orderRepository;
+
+    /**
+     * ManyToOne, OneToOne
+     * Order -> Member
+     * Order -> Delivery
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public SimpleOrdersResult ordersV2() {
+        List<SimpleOrderDTO> all =
+                orderRepository.findAll(new OrderSearch())
+                        .stream().map(SimpleOrderDTO::new)
+                        .collect(Collectors.toList());
+        return new SimpleOrdersResult(all);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class SimpleOrdersResult {
+        List<SimpleOrderDTO> orders;
+    }
+
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        SimpleOrderDTO(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
